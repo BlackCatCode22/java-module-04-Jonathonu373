ac80a274f2e5c3589fe279e80caa2cd0824bc8f6
Jonathonu373 <uribejonathon@gmail.com>
2024-10-03 11:59:29 -0700

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -1,36 +1,73 @@
+package dennis.zoo.com;
+
 import java.util.Date;
 
 public class Animal {
-
-
-
-    // Static int to keep track of the number of animals created
+    // Create a static int that keeps track of the number of animals created.
     static int numOfAnimals = 0;
 
-    // Attributes (fields)
-    private String sex; // Sex will be 'male' or 'female'
-    private int age = 0; // Age in years
-    private int weight = 0; // Weight in pounds
-    private String animalID; // Unique ID for each animal
-    private String animalName; // Name of the animal
-    // origin zoo
-    private String animalOrigin;
-    // animal color
-    private String getAnimalColor;
-    // arrival Date
-    private Date animalArrivalDate;
-
-
-    // Constructor
+    // Create a constructor for our new Animal object
     public Animal() {
         numOfAnimals++;
-    // Create a constructor that accepts a name
-    public Animal(String aName) {
-        }
-        this.animalName = aName;
     }
 
-    // Getters and setters
+    // Create a constructor that will set all fields as arguments.
+    public Animal (String sex, int age, int weight, String animalName,
+                 String animalID, String animalBirthdate, String animalColor, String animalOrigin) {
+        this.sex = sex;
+        this.age = age;
+        this.weight = weight;
+        this.animalName = animalName;
+        this.animalID = animalID;
+        this.animalBirthdate = animalBirthdate;
+        this.animalColor = animalColor;
+        this.animalOrigin = animalOrigin;
+    }
+
+
+
+    // Create all attributes (fields) for our midterm program
+    // Sex will be 'male' or 'female'
+    String sex;
+    // age will be in years
+    private int age;
+    // weight will be in pounds
+    private int weight;
+    // name will be a string;
+    String animalName;
+    // animalID
+    private String animalID;
+    // animalBirthdate is going to be a string in class
+    private String animalBirthdate;
+    // animalColor is a string
+    private String animalColor;
+    // origin will be a string like: "from Frigula Park, Tunisia"
+    private String animalOrigin;
+
+
+
+
+    // Create getters and setters
+    public String getAnimalName() {
+        return animalName;
+        }
+    public String getAnimalOrigin() {
+            return animalOrigin;
+        }
+
+    public String getAnimalBirthdate() {
+        return animalBirthdate;
+    }
+
+    public String getAnimalColor(){
+        return animalColor;
+    }
+
+
+
+
+
+
     public int getAge() {
         return age;
     }
@@ -47,47 +84,11 @@ public class Animal {
         this.sex = sex;
     }
 
-    public int getWeight() {
-        return weight;
-    }
-
     public void setWeight(int weight) {
         this.weight = weight;
     }
 
-    public String getAnimalID() {
-        return animalID;
+    public int getWeight() {
+        return weight;
     }
-
-    public void setAnimalID(String animalID) {
-        this.animalID = animalID;
-    }
-
-    public String getAnimalName() {
-        return animalName;
-    }
-
-    public void setAnimalName(String animalName) {
-        this.animalName = animalName;
-    }
-
-    public Date getAnimalBirthdate() {return AnimalBirthdate;}
-    public void setAnimalbirthdate(Date animalbirthdate) {}
-
-    public String getGetAnimalColor() {return animalColor;}
-    public void setGetAnimalColor(String animalColor) {this.animalColor = animalColor;}
-
-    public String getAnimalOrigin() {return animalOrigin;}
-    public void setAnimalOrigin(String animalOrigin) {this.animalOrigin = animalOrigin;}
-
-    public Date getAnimalArrivalDate() {return animalArrivalDate;}
-    public void setAnimalArrivalDate(Date animalArrivalDate) {this.animalArrivalDate = animalArrivalDate;}
-
-
-
-
-
-
-
-
 }
---
+++ Hyena.java
@@ -0,0 +1,7 @@
+package dennis.zoo.com;
+
+import dennis.zoo.com.Animal;
+
+public class Hyena extends dennis.zoo.com.Animal {
+    // create a static int that keep track
+}
